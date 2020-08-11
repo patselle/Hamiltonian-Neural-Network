6a0f3ef53db875d5d2043c54e35cede8444ca97b
ps101 <patrick.selle@hahn-schickard.de>
2020-08-11 15:46:00 +0200

    add particles

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 
 #include "particle.h"
 
+#define N 3
+
 
 static inline float frand()
 {
@@ -21,5 +23,22 @@ int main()
 
     a.x = 3.0;
 
+    static particle particles[N];
+
     printf("random number in [0,1) %f\n", frand());
+
+    for (int a = 0; a < N; a++)
+    {
+        printf("it %d\n", N);
+
+        // init position
+        particles[a].x = frand();
+        particles[a].y = frand();
+        particles[a].z = frand();
+
+        // init momentum
+        particles[a].px = frand();
+        particles[a].py = frand();
+        particles[a].pz = frand();
+    }
 }
--- src/particle.h
+++ src/particle.h
@@ -5,6 +5,8 @@ typedef struct
 {
     float x,y,z;
     float px,py,pz;
+    float mass;
+    float radius;
 }particle;
 
 #endif
