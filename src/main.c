5a358dbef56468455de38a72fe82d68eb703c08f
pascal <pascal@mint.mshome.net>
2020-08-11 16:27:55 +0200

    added background threading

--- src/main.c
+++ src/main.c
@@ -1,17 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 
 #include "particle.h"
 #include "graphics.h"
 
 #define N 10
 
+static particle particles[N];
+
 static inline float frand()
 {
     return (float)rand() / RAND_MAX;
 }
 
-static void particle_init(particle *p)
+static void particle_init()
 {
     int i;
 
@@ -19,18 +23,45 @@ static void particle_init(particle *p)
 
     for (i = 0; i < N; i++)
     {
-        p[i].x = (rand() % 50) - 25;
-        p[i].y = (rand() % 50) - 25;
+        particles[i].x = (rand() % 50) - 25;
+        particles[i].y = (rand() % 50) - 25;
+        particles[i].px = frand() / 100;
+        particles[i].py = frand() / 100;
     }    
+}
+
+static void *particle_move(void *args)
+{
+    int i;
+
+    while (1)
+    {
+        // here compute particle movement
+
+        for (i = 0; i < N; i++)
+        {
+            particles[i].x += particles[i].px;
+            particles[i].y += particles[i].py;
+        }
+
+        graphics_draw(particles, N);
+
+        // 13 milliseconds
+        usleep(13 * 1000);
+    }
 }
 
 int main()
 {
-    particle p[N];
+    pthread_t thread;
 
-    particle_init(p);
+    particle_init();
 
-    graphics_init();
-    graphics_draw(p, N);
+    pthread_create(&thread, NULL, particle_move, NULL);
+
+    graphics_init();    
     graphics_loop();
+
+    pthread_cancel(thread);
+    pthread_join(thread, NULL);
 }
