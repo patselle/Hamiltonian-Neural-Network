7ae897fdc51302659837b544c832cba3b77dd50f
ps101 <patrick.selle@hahn-schickard.de>
2020-08-11 15:17:25 +0200

    add frand()

--- src/main.c
+++ src/main.c
@@ -3,11 +3,23 @@
 
 #include "particle.h"
 
+
+static inline float frand()
+{
+    return (float)rand() / RAND_MAX;
+}
+
 int main()
 {
     printf("Hello from main\n");
 
-    printf("Create the first particle\n");
+    printf("Create some particles\n");
 
+    particle a;
     particle b;
+    particle c;
+
+    a.x = 3.0;
+
+    printf("random number in [0,1) %f\n", frand());
 }
